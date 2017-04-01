fa316a1a89ae7bdad0f3972b2c3aba7957ca8c51
Connor McElroy <cmcelroy@ksu.edu>
2017-04-01 17:43:26 -0500

    Not quite working, please look at.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,22 @@
+function loadIndex() {
+  $.get('/projects', function(projects, status){
+    if(status == 200) {
+      $('body').clear();
+      projects.forEach(function(project){
+        var link = $('a')
+          .text(project.name)
+          .attr('href', '/projects/' + project.id)
+          .on('click', function(e){
+            e.preventDefault();
+            loadProject('/projects/' + project.id);
+          });
+        $('body').append(link);
+      })
+    }
+  });
+}
+
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '/projects/');
 xhr.send(null);
@@ -8,8 +27,22 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
       console.log(xhr.responseText); // 'This is the returned text.'
+      var projects = JSON.parse(xhr.responseText);
+      projects.forEach(function(project){
+        var name = document.createElement('a');
+        name.innerHTML = project.name;
+        name.href = "/projects/" + project.id;
+        document.body.appendChild(name);
+        project.onClick = function(event) {
+          event.preventDefault();
+          alert("Load using Ajax");
+        }
+      });
+
     } else {
       console.log('Error: ' + xhr.status); // An error occurred during the request.
     }
   }
 }
+
+loadIndex();
